95b1296c4eee38b491d4ca6dc7843c7f0e65455b
karmstr0ng <kevinarmstr0ng@hotmail.com>
2020-06-13 13:32:12 -0700

    First commit

---
+++ Employee DB Application/Employee.cpp
@@ -0,0 +1,26 @@
+/*
+ * Employee.cpp
+ *
+ *  Created on: Jun 12, 2020
+ *      Author: kevin
+ */
+
+
+#include <iostream>
+#include <sstream>
+#include "employee.h"
+
+using namespace std;
+
+Person::Person() {
+	cout << "This is the Person constructor" << endl;
+
+}
+
+Person::~Person() {
+	cout << "This is the Person destructor" << endl;
+}
+
+Person::setEmp_no(string number) {
+
+}
